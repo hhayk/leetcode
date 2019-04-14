3be2d750f4670ae120119d4d0c6c2c62b350eb23
hhayk <hayk.hakobyan87@gmail.com>
2019-04-14 12:19:45 +0400

    884. Uncommon Words from Two Sentences

---
+++ 884.Uncommon Words from Two Sentences.js
@@ -0,0 +1,41 @@
+/*
+We are given two sentences A and B.  (A sentence is a string of space separated words.  Each word consists only of lowercase letters.)
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Return a list of all uncommon words. 
+
+You may return the list in any order.
+
+Example 1:
+
+Input: A = "this apple is sweet", B = "this apple is sour"
+Output: ["sweet","sour"]
+Example 2:
+
+Input: A = "apple apple", B = "banana"
+Output: ["banana"]
+*/
+
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {string[]}
+ */
+var uncommonFromSentences = function (A, B) {
+    let map = new Map();
+    let func = (str) => str.split(" ").forEach(e => map.set(e, (map.get(e) ? map.get(e) : 0) + 1));
+
+    func(A);
+    func(B);
+
+    let ret = [];
+    for (const [key, value] of map.entries()) {
+        if (value == 1) ret.push(key);
+    }
+
+    return ret;
+};
+
+console.log(uncommonFromSentences("this apple is sweet", "this apple is sour"));
+console.log(uncommonFromSentences("apple apple", "banana"));
