56bf93bb082fc8c9e47f3e99fa4b0abb15794988
hhayk <hayk.hakobyan87@gmail.com>
2019-04-16 01:50:43 +0400

    477. Total Hamming Distance

---
+++ 477. Total Hamming Distance.js
@@ -0,0 +1,60 @@
+/*
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Now your job is to find the total Hamming distance between all pairs of the given numbers.
+
+Example:
+    Input: 4, 14, 2
+
+    Output: 6
+
+Explanation: 
+    In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+    showing the four bits relevant in this case). So the answer will be:
+    HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+
+Note:
+    Elements of the given array are in the range of 0 to 10^9
+    Length of the array will not exceed 10^4.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var totalHammingDistance = function (nums) {
+    // let hammingDistance = (a, b) => {
+    //     let sum = 0;
+    //     let xor = a ^ b;
+    //     while (xor > 0) {
+    //         sum += xor & 1;
+    //         xor = xor >> 1;
+    //     }
+
+    //     return sum;
+    // }
+
+    // let sum = 0;
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         sum += hammingDistance(nums[i], nums[j]);
+    //     }
+    // }
+
+    // return sum;
+
+    let sum = 0;
+    for (let i = 0; i < 32; i++) {
+        let bitCount = 0;
+        for (let n of nums) {
+            bitCount += (n >> i) & 1;
+        }
+        sum += bitCount * (nums.length - bitCount);
+    }
+    return sum;
+};
+
+console.log(totalHammingDistance([4, 14, 2]));
+console.log(totalHammingDistance([6, 1, 8, 6, 8]));
