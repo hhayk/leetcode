2fc0acdd5b7e05cc8e4aeeca10e3aad72c9676c6
hhayk <hayk.hakobyan87@gmail.com>
2019-04-16 06:32:05 +0400

    876. Middle of the Linked List

---
+++ 876. Middle of the Linked List.js
@@ -0,0 +1,79 @@
+/*
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+    Input: [1,2,3,4,5]
+    Output: Node 3 from this list (Serialization: [3,4,5])
+    The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+    Note that we returned a ListNode object ans, such that:
+    ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+
+Example 2:
+
+    Input: [1,2,3,4,5,6]
+    Output: Node 4 from this list (Serialization: [4,5,6])
+    Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Note:
+
+    The number of nodes in the given list will be between 1 and 100.
+
+*/
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+    let arr = []
+
+    while (head) {
+        let temp = head;
+        arr.push(temp);
+
+        head = head.next;
+    }
+
+    return arr[Math.floor(arr.length / 2)];
+};
+
+{
+    let node = new ListNode(1)
+    node.next = new ListNode(2);
+    node.next.next = new ListNode(3);
+    node.next.next.next = new ListNode(4);
+    node.next.next.next.next = new ListNode(5);
+
+    console.log(middleNode(node));
+}
+
+{
+    let node = new ListNode(1)
+    node.next = new ListNode(2);
+    node.next.next = new ListNode(3);
+    node.next.next.next = new ListNode(4);
+    node.next.next.next.next = new ListNode(5);
+    node.next.next.next.next.next = new ListNode(6);
+
+    console.log(middleNode(node));
+}
