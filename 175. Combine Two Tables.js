0f8f327450fd1c6a166d92adf80ab5efb247df15
hhayk <hayk.hakobyan87@gmail.com>
2019-04-16 06:20:47 +0400

    175. Combine Two Tables

---
+++ 175. Combine Two Tables.js
@@ -0,0 +1,13 @@
+/*
+
+# Write your MySQL query statement below
+
+*/
+
+/*
+
+    SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+    FROM Person
+    LEFT JOIN Address ON Person.PersonId = Address.PersonId
+
+*/
