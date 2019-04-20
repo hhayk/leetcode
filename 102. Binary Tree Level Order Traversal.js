17e47170a3ee4bb94ecc799ca791458ce0b04f7a
hhayk <hayk.hakobyan87@gmail.com>
2019-04-20 06:38:32 +0400

    102. Binary Tree Level Order Traversal

---
+++ 102. Binary Tree Level Order Traversal.js
@@ -0,0 +1,65 @@
+/*
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+    [
+        [3],
+        [9,20],
+        [15,7]
+    ]
+
+*/
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    let acc = [];
+    let stack = [];
+
+    stack.push([root, 0]);
+    while (stack.length > 0) {
+        let tuple = stack.shift();
+        let node = tuple[0];
+        let level = tuple[1];
+
+        if (!acc[level]) acc[level] = [];
+        acc[level].push(node.val);
+
+        if (node.left) stack.push([node.left, level + 1]);
+        if (node.right) stack.push([node.right, level + 1]);
+    }
+
+    return acc;
+};
+
+{
+    let node = new TreeNode(3);
+    node.left = new TreeNode(9);
+    node.right = new TreeNode(20);
+    node.right.left = new TreeNode(15);
+    node.right.right = new TreeNode(7);
+
+    console.log(levelOrder(node));
+}
