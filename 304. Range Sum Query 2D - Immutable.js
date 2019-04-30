a547f7ac1501aa624934a19c7c2a3374d6c7db6b
hhayk <hayk.hakobyan87@gmail.com>
2019-04-30 13:57:01 +0400

    304. Range Sum Query 2D - Immutable

---
+++ 304. Range Sum Query 2D - Immutable.js
@@ -0,0 +1,72 @@
+/*
+
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+
+*/
+
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function (matrix) {
+    this.matrix = matrix;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+    let sum = 0;
+
+    for (let row = row1; row <= row2; row++) {
+        for (let col = col1; col <= col2; col++) {
+            sum += this.matrix[row][col];
+        }
+    }
+
+    return sum;
+};
+
+/** 
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+
+{
+    let obj = new NumMatrix(
+        [
+            [3, 0, 1, 4, 2],
+            [5, 6, 3, 2, 1],
+            [1, 2, 0, 1, 5],
+            [4, 1, 0, 1, 7],
+            [1, 0, 3, 0, 5]
+        ]
+    );
+    console.log(obj.sumRegion(2, 1, 4, 3));
+    console.log(obj.sumRegion(1, 1, 2, 2));
+    console.log(obj.sumRegion(1, 2, 2, 4));
+}
