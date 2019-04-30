a0a7cd466be6a06d8f075ef6e05b8d747ad60daa
hhayk <hayk.hakobyan87@gmail.com>
2019-04-30 13:42:07 +0400

    202. Happy Number

---
+++ 202. Happy Number.js
@@ -0,0 +1,36 @@
+/*
+
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+    let set = new Set();
+    let isHappyRec = (n) => {
+        if (set.has(n)) return false;
+        set.add(n);
+
+        let sum = [...n.toString()].map(i => Number(i)).reduce((a, b) => a + b * b, 0);
+        return sum == 1 ? true : isHappyRec(sum);
+    }
+
+    return isHappyRec(n);
+};
+
+console.log(isHappy(19));
