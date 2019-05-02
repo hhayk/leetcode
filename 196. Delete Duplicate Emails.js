64ada1f956bc171aab7f1bbcc008f429919b9f15
hhayk <hayk.hakobyan87@gmail.com>
2019-05-02 16:57:47 +0400

    196. Delete Duplicate Emails

---
+++ 196. Delete Duplicate Emails.js
@@ -0,0 +1,29 @@
+/*
+
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Note:
+
+Your output is the whole Person table after executing your sql. Use delete statement.
+
+*/
+
+DELETE T1
+FROM Person T1, Person T2
+WHERE T1.Email = T2.Email AND T1.Id > T2.Id
