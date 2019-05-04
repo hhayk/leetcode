acbd6674600fd945b8d5c8df9cfbc93bd9913e56
hhayk <hayk.hakobyan87@gmail.com>
2019-05-04 17:48:52 +0400

    713. Subarray Product Less Than K

---
+++ 713. Subarray Product Less Than K.js
@@ -0,0 +1,41 @@
+/*
+
+Your are given an array of positive integers nums.
+
+Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+
+Example 1:
+Input: nums = [10, 5, 2, 6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Note:
+
+0 < nums.length <= 50000.
+0 < nums[i] < 1000.
+0 <= k < 10^6.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function (nums, k) {
+    if (k <= 1) return 0;
+
+    let cnt = 0;
+    let prod = 1;
+    let left = 0
+    for (let right = 0; right < nums.length; right++) {
+        prod *= nums[right];
+        while (prod >= k) prod /= nums[left++];
+        cnt += right - left + 1;
+    }
+
+    return cnt;
+};
+
+console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
+console.log(numSubarrayProductLessThanK([10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3], 19));
