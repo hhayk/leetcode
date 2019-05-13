b2d260b0cbeb7976e073f0c382c8328eba0450c4
hhayk <hayk.hakobyan87@gmail.com>
2019-05-13 17:33:25 +0400

    917. Reverse Only Letters

---
+++ 917. Reverse Only Letters.js
@@ -0,0 +1,58 @@
+/*
+
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+ 
+
+Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+Note:
+
+S.length <= 100
+33 <= S[i].ASCIIcode <= 122 
+S doesn't contain \ or "
+
+*/
+
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function (S) {
+    let arr = [...S];
+    let left = 0;
+    let right = arr.length - 1;
+
+    let isLetter = (ch) => ch.match(/[a-z]/i);
+
+    while (left < right) {
+        if (!isLetter(arr[left])) left++;
+        else if (!isLetter(arr[right])) right--;
+        else {
+            let tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+
+            left++;
+            right--;
+        }
+    }
+
+    return arr.join("");
+};
+
+console.log(reverseOnlyLetters("ab-cd"));
+console.log(reverseOnlyLetters("a-bC-dEf-ghIj"));
+console.log(reverseOnlyLetters("Test1ng-Leet=code-Q!"));
