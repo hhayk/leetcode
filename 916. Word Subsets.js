d58b60436405d67f6bababbb48fac913feca0332
hhayk <hayk.hakobyan87@gmail.com>
2019-05-13 18:26:37 +0400

    916. Word Subsets

---
+++ 916. Word Subsets.js
@@ -0,0 +1,70 @@
+/*
+
+We are given two arrays A and B of words.  Each word is a string of lowercase letters.
+
+Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
+
+Now say a word a from A is universal if for every b in B, b is a subset of a. 
+
+Return a list of all universal words in A.  You can return the words in any order.
+
+Example 1:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","o"]
+Output: ["facebook","google","leetcode"]
+Example 2:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["l","e"]
+Output: ["apple","google","leetcode"]
+Example 3:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","oo"]
+Output: ["facebook","google"]
+Example 4:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["lo","eo"]
+Output: ["google","leetcode"]
+Example 5:
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["ec","oc","ceo"]
+Output: ["facebook","leetcode"]
+ 
+
+Note:
+
+1 <= A.length, B.length <= 10000
+1 <= A[i].length, B[i].length <= 10
+A[i] and B[i] consist only of lowercase letters.
+All words in A[i] are unique: there isn't i != j with A[i] == A[j].
+
+*/
+
+/**
+ * @param {string[]} A
+ * @param {string[]} B
+ * @return {string[]}
+ */
+var wordSubsets = function (A, B) {
+    let aCharCode = 'a'.charCodeAt(0);
+    let arr = Array(26).fill(0);
+
+    B.forEach(b => {
+        let tmp = Array(26).fill(0);
+        [...b].forEach(ch => tmp[ch.charCodeAt(0) - aCharCode]++);
+        arr.forEach((_, i) => arr[i] = Math.max(arr[i], tmp[i]));
+    });
+
+    let isSubset = (w) => {
+        let tmp = Array(26).fill(0);
+        [...w].forEach(ch => tmp[ch.charCodeAt(0) - aCharCode]++);
+        return tmp.every((_, i) => tmp[i] >= arr[i]);
+    }
+
+    return A.filter(word => isSubset(word));
+};
+
+console.log(wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "o"]));
+console.log(wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["l", "e"]));
+console.log(wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "oo"]));
+console.log(wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["lo", "eo"]));
+console.log(wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["ec", "oc", "ceo"]));
