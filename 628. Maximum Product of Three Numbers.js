9164c92e09febd114f330cf6edd2e50450d3aa0b
hhayk <hayk.hakobyan87@gmail.com>
2019-05-20 18:29:29 +0400

    628. Maximum Product of Three Numbers

--- 628. Maximum Product of Three Numbers.js
+++ 628. Maximum Product of Three Numbers.js
@@ -27,6 +27,7 @@ Multiplication of any three numbers in the input won't exceed the range of 32-bi
  */
 var maximumProduct = function (nums) {
     nums.sort((a, b) => a - b);
+    
     let len = nums.length;
     let p1 = nums[0] * nums[1] * nums[len - 1];
     let p2 = nums[len - 3] * nums[len - 2] * nums[len - 1];
