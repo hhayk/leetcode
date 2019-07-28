374359f8d99503a0d693489725814080b879b599
hhayk <hayk.hakobyan87@gmail.com>
2019-07-28 16:37:21 +0400

    1. Two Sum

---
+++ 001. Two Sum.py
@@ -0,0 +1,28 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+
+        for index, value in enumerate(nums):
+            if target - value in map:
+                return [map[target - value], index]
+            map[value] = index
+
+
+print(Solution().twoSum([2, 7, 11, 15], 9))
